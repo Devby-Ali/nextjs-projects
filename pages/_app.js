6edf719030dc9bd9e9652e1bca66a121146ebf12
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-01 15:07:42 +0330

    Make page layouts (footer & header)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="container">
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
