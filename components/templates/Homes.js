acd473cdedb9ffecfd1396e1ff2e7330e76e3e6a
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-01 15:21:16 +0330

    Make home box components

--- components/templates/Homes.js
+++ components/templates/Homes.js
@@ -1,12 +1,14 @@
 import React from "react";
+import db from "../../data/db.json";
+import Home from "@/components/modules/Home";
 
 function Homes() {
+  console.log(db);
   return (
     <div className="homes">
-      <div className="card">home</div>
-      <div className="card">home</div>
-      <div className="card">home</div>
-      <div className="card">home</div>
+      {db.homes.slice(0, 6).map((home) => (
+        <Home key={home.id} {...home} />
+      ))}
     </div>
   );
 }
