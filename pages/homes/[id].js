c89051a2ef98f6bc42b6a7b659677327e7430e38
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-01 14:57:00 +0330

    Make Routing (Page router)

---
+++ pages/homes/[id].js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SingleHome() {
+
+  return <div >single home page</div>;
+}
+
+export default SingleHome;
---
+++ pages/homes/index.js
@@ -0,0 +1,10 @@
+function index() {
+
+  return (
+    <div className="home-section" id="houses">
+        all homes page
+    </div>
+  );
+}
+
+export default index;
