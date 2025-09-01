2f4131cad29e85c4840af1c666275d1688b182b0
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-01 15:42:16 +0330

    Development of all house route

--- components/templates/Homes.js
+++ components/templates/Homes.js
@@ -3,7 +3,7 @@ import db from "../../data/db.json";
 import Home from "@/components/modules/Home";
 
 function Homes() {
-  console.log(db);
+  
   return (
     <div className="homes">
       {db.homes.slice(0, 6).map((home) => (
--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,8 +1,47 @@
+import Home from "@/components/modules/Home";
+import db from "../../data/db.json";
 function index() {
 
   return (
     <div className="home-section" id="houses">
-        all homes page
+      <div className="home-filter-search">
+        <div className="home-filter">
+          <select>
+            <option value="-1">انتخاب کنید</option>
+            <option value="price">بر اساس قیمت</option>
+            <option value="room">بر اساس تعداد اتاق</option>
+            <option value="meterage">بر اساس اندازه</option>
+          </select>
+        </div>
+        <div className="home-search">
+          <input
+            type="text"
+            placeholder="جستجو کنید"
+          />
+        </div>
+      </div>
+
+      <div className="homes">
+        {db.homes.map((home) => (
+          <Home key={home.id} {...home} />
+        ))}
+      </div>
+
+      <ul className="pagination__list">
+        {Array.from({ length: Math.ceil(db.homes.length / 3) }).map(
+          (item, index) => (
+            <li
+              key={index + 1}
+              className="pagination__item"
+              onClick={(event) => paginateHandler(event, index + 1)}
+            >
+              <a href="#" className="">
+                {index + 1}
+              </a>
+            </li>
+          )
+        )}
+      </ul>
     </div>
   );
 }
