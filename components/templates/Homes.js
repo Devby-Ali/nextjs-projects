1e88c4e150e568040ec92bfda43e978af4ac4f32
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-01 15:15:57 +0330

    Make templates

---
+++ components/templates/Homes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Homes() {
+  return (
+    <div className="homes">
+      <div className="card">home</div>
+      <div className="card">home</div>
+      <div className="card">home</div>
+      <div className="card">home</div>
+    </div>
+  );
+}
+
+export default Homes;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-export default function Home() {
+import Features from "@/components/templates/Features";
+import Gallery from "@/components/templates/Gallery";
+import Homes from "@/components/templates/Homes";
+import Story from "@/components/templates/Story";
+import React from "react";
+
+function index() {
   return (
     <>
-      <div>index page</div>
+      <Features />
+      <Story />
+      <Homes />
+      <Gallery />
     </>
   );
 }
+
+export default index;
