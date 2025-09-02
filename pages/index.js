7d0d593e65541f20c8229fcfb1c3d188fb15bb16
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-02 23:20:23 +0330

    Make About Template for Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Slider from "@/components/templates/Index/Slider";
 import React from "react";
+import About from "@/components/templates/Index/About";
 
 function Index() {
   return (
     <>
       <Slider />
+      <About />
     </>
   );
 }
