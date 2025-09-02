dcb8b28c793b88afba089637516ca829fdf46746
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-02 23:31:39 +0330

    Fetching cafe services from the server

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,28 @@ import React from "react";
 import About from "@/components/templates/Index/About";
 import Services from "@/components/templates/Index/Services";
 
-function Index() {
+function Index({ data }) {
   return (
     <>
       <Slider />
       <About />
-      <Services />
+      <Services services={data.services} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:4000/services");
+  const services = await res.json();
+
+  return {
+    props: {
+      data: {
+        services,
+      },
+    },
+    revalidate: 60 * 60 * 12, // Second
+  };
 }
 
 export default Index;
