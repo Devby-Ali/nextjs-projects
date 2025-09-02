a9c580e1e75233972ad862cb87cc648eca9b574c
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-02 22:58:53 +0330

    Make folder structure (start nextCoffee project)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,5 @@
 import "@/styles/globals.css";
-import Footer from "@/components/layout/Footer";
-import Header from "@/components/layout/Header";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div className="container">
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
-import Features from "@/components/templates/Features";
-import Gallery from "@/components/templates/Gallery";
-import Homes from "@/components/templates/Homes";
-import Story from "@/components/templates/Story";
 import React from "react";
 
-function index() {
-  return (
-    <>
-      <Features />
-      <Story />
-      <Homes />
-      <Gallery />
-    </>
-  );
+function Index() {
+  return <h1>Coffe Home Page :))</h1>;
 }
 
-export default index;
+export default Index;
