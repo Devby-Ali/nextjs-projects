4e102e566763f1ebf08f25ae06d4ac0bd5235d28
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-02 23:22:33 +0330

    Make Services Template for Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Slider from "@/components/templates/Index/Slider";
 import React from "react";
 import About from "@/components/templates/Index/About";
+import Services from "@/components/templates/Index/Services";
 
 function Index() {
   return (
     <>
       <Slider />
       <About />
+      <Services />
     </>
   );
 }
