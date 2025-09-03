f8678229cc428b52ff44ae453e6845ff080f519b
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-03 18:04:24 +0330

    Make Cafe menu / reservation template

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import About from "@/components/templates/Index/About";
 import Services from "@/components/templates/Index/Services";
 import Offer from "@/components/templates/Index/Offer";
+import Menu from "@/components/templates/Index/Menu";
+import Reservation from "@/components/templates/Index/reservation";
 
 function Index({ data }) {
   return (
@@ -11,18 +13,24 @@ function Index({ data }) {
       <About />
       <Services services={data.services} />
       <Offer />
+      <Menu data={data.menu} />
+      <Reservation />
     </>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:4000/services");
-  const services = await res.json();
+  const servicesResponse = await fetch("http://localhost:4000/services");
+  const servicesData = await servicesResponse.json();
+
+  const menuResponse = await fetch("http://localhost:4000/menu");
+  const menuData = await menuResponse.json();
 
   return {
     props: {
       data: {
-        services,
+        services: servicesData,
+        menu: menuData,
       },
     },
     revalidate: 60 * 60 * 12, // Second
