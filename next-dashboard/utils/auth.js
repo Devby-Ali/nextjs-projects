f75c10850c40698a9d8e2081c5cce589849a4b1f
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-07 13:33:30 +0330

    Add cookie & Remove comments

--- next-dashboard/utils/auth.js
+++ next-dashboard/utils/auth.js
@@ -2,14 +2,12 @@ import { hash, compare } from "bcryptjs";
 import { sign, verify } from "jsonwebtoken";
 
 const hashPassword = async (password) => {
-  // password = ali1212 => Hash => dngsbipnrg9ipbn39ubnj9unertn
   const hashedPassword = await hash(password, 12);
   return hashedPassword;
 };
 
 const generateToken = (data) => {
   const token = sign({ ...data }, process.env.privateKey, {
-    // algorithm: ''
     expiresIn: "24h",
   });
 
