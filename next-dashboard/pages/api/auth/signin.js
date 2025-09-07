57bc312176efcaaeff2d60fa915cf2434726ade7
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-07 11:29:11 +0330

    Password verification and token generation

--- next-dashboard/pages/api/auth/signin.js
+++ next-dashboard/pages/api/auth/signin.js
@@ -1,6 +1,6 @@
 import UserModel from "@/models/User";
 import connectToDB from "@/configs/db";
-import { generateToken } from "@/utils/auth";
+import { generateToken, verifyPassword } from "@/utils/auth";
 import { serialize } from "cookie";
 
 const handler = async (req, res) => {
@@ -25,9 +25,31 @@ const handler = async (req, res) => {
       return res.status(404).json({ message: "User not found !!" });
     }
 
-    // Codes
+    const isValidPassword = await verifyPassword(password, user.password);
+
+    if (!isValidPassword) {
+      return res
+        .status(422)
+        .json({ message: "username or password is not correct !!" });
+    }
+
+    const token = generateToken({ email: user.email });
+
+    return res
+      .setHeader(
+        "Set-Cookie",
+        serialize("token", token, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 60 * 24,
+        })
+      )
+      .status(200)
+      .json({ message: "User Logged In Successfully :))" });
   } catch (err) {
-    // Codes
+    return res
+      .status(500)
+      .json({ message: "UnKnown Internal Server Erorr !!" });
   }
 };
 
