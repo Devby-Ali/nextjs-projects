aaf070b544031ff5b80d72cfe6f2fd373f70e5b8
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-07 11:50:26 +0330

    GetMe Api development

--- next-dashboard/pages/api/auth/me.js
+++ next-dashboard/pages/api/auth/me.js
@@ -1,5 +1,39 @@
-const handler = (req, res) => {
-  // Codes
+import connectToDB from "@/configs/db";
+import { verifyToken } from "@/utils/auth";
+
+const handler = async (req, res) => {
+  if (req.method !== "GET") {
+    return false;
+  }
+
+  try {
+    connectToDB();
+
+    const { token } = req.cookies;
+
+    if (!token) {
+      return res.status(401).json({ message: "You are not login !!" });
+    }
+
+    const tokenPayload = verifyToken(token);
+
+    if (!tokenPayload) {
+      return res.status(401).json({ message: "You are not login !!" });
+    }
+
+    const user = await UserModel.findOne(
+      {
+        email: tokenPayload.email,
+      },
+      "firstname lastname role"
+    );
+
+    return res.status(200).json({ data: user });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "UnKnown Internal Server Erorr !!" });
+  }
 };
 
 export default handler;
