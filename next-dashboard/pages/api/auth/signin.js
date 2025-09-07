634ecba8d5a106a82a80bf9cd6ee6e3866f3f4f0
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-07 11:26:44 +0330

    Make User Login API

--- next-dashboard/pages/api/auth/signin.js
+++ next-dashboard/pages/api/auth/signin.js
@@ -1,5 +1,34 @@
-const handler = (req, res) => {
-  // Codes
+import UserModel from "@/models/User";
+import connectToDB from "@/configs/db";
+import { generateToken } from "@/utils/auth";
+import { serialize } from "cookie";
+
+const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return false;
+  }
+
+  try {
+    connectToDB();
+
+    const { identifier, password } = req.body;
+
+    if (!identifier.trim() || !password.trim()) {
+      return res.status(422).json({ message: "Data is not valid !!" });
+    }
+
+    const user = await UserModel.findOne({
+      $or: [{ username: identifier }, { email: identifier }],
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found !!" });
+    }
+
+    // Codes
+  } catch (err) {
+    // Codes
+  }
 };
 
 export default handler;
