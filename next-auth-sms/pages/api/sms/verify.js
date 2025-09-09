6bb5ba0389e812055558b053748c1d151b367bdb
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-09-09 13:42:06 +0330

    Make API to verify the code received from the user

---
+++ next-auth-sms/pages/api/sms/verify.js
@@ -0,0 +1,26 @@
+import otpModel from "@/models/otp";
+
+const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return false;
+  }
+
+  const { phone, code } = req.body;
+
+  const otp = await otpModel.findOne({ phone, code });
+
+  if (otp) {
+    const date = new Date();
+    const now = date.getTime();
+
+    if (otp.expTime > now) {
+      return res.status(200).json({ message: "Code is correct :))" });
+    } else {
+      return res.status(410).json({ message: "Code is expired !!" });
+    }
+  } else {
+    return res.status(409).json({ message: "Code is not correct !!" });
+  }
+};
+
+export default handler;
